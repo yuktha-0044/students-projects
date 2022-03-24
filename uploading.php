fa3064db157e2011cfd6ba0f98e20baf6898a3d2
jhansi20 <63865253+jhansi20@users.noreply.github.com>
2022-03-24 16:06:34 +0530

    Update uploading.php

--- uploading.php
+++ uploading.php
@@ -1,31 +1,71 @@
+<?php
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Upload File</title>
-</head>
-<body>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Online Project submission</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+<div class="header">
+    <nav>
+        <div class="logo">
+           <img src="images/logo.png">
+           <span>Techies</span>
+        </div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="uploading.php">Upload Project</a></li>
+            <li><a href="search_project.php">Search Project</a></li>
+            <li><a href="#">Check Plagiarism</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+    <center>
     <form method="post" action="submit.php" enctype="multipart/form-data">
-        <table>
+        <table cellpadding="3" cellspacing="10">
             <tr>
-                <td>student name:</td>
+                <td>Student Name:</td>
                 <td><input type="text" name="sname"></td>
             </tr>
             <tr>
-                <td>College name:</td>
+                <td></td>
+                <td></td>
+          </tr>
+            <tr>
+                <td>College Name:</td>
                 <td><input type="text" name="cname"></td>
             </tr>
+            <tr>
+                <td></td>
+                <td></td>
+          </tr>
             <tr>
                 <td>Project Title:</td>
                 <td><input type="text" name="ptitle"></td>
             </tr>
+            <tr>
+                <td></td>
+                <td></td>
+          </tr>
             <tr>
                 <td>Upload File:</td>
                 <td><input type="file" name="fileUpload"></td>
             </tr>
             <tr>
                 <td></td>
+                <td></td>
+          </tr>
+            <tr>
+                <td></td>
                 <td><input type="submit" value="Submit"></td>
             </tr>
         </table>
     </form>
-</body>
-    </html>
+    </div>
+</div>
+
+        
+    </body>
+</html>
+                    ?>
